62c26a33734efb1acc6dfe0d9cae169696ae8aef
Quinn Austin <63010095+AustinQ23@users.noreply.github.com>
2023-01-26 13:18:35 -0800

    Add files via upload

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -42,11 +42,13 @@ gTextures = {
     ['character-swing-sword'] = love.graphics.newImage('graphics/character_swing_sword.png'),
     ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
     ['switches'] = love.graphics.newImage('graphics/switches.png'),
-    ['entities'] = love.graphics.newImage('graphics/entities.png')
+    ['entities'] = love.graphics.newImage('graphics/entities.png'),
+    ['water-tiles'] = love.graphics.newImage('graphics/tileset_water.png')
 }
 
 gFrames = {
     ['tiles'] = GenerateQuads(gTextures['tiles'], 16, 16),
+    ['water-tiles'] = GenerateQuads(gTextures['water-tiles'], 16, 16),
     ['character-walk'] = GenerateQuads(gTextures['character-walk'], 16, 32),
     ['character-swing-sword'] = GenerateQuads(gTextures['character-swing-sword'], 32, 32),
     ['entities'] = GenerateQuads(gTextures['entities'], 16, 16),
